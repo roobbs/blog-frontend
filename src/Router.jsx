56652631e7a93773735e24910412b6a482b113ce
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-06-16 17:02:14 -0600

    Added header component styled

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,22 +3,6 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Index from "./screens/Index";
 import Home from "./screens/Home";
 
-// export default function Router() {
-//   const router = createBrowserRouter([
-//     {
-//       path: "/",
-//       element: <Index />,
-//       // errorElement: <ErrorScreen/>,
-//       children: [
-//         { index: true, element: <Home /> },
-//         // {path:"post", element:<Home/>}
-//       ],
-//     },
-//   ]);
-
-//   return <RouterProvider router={router} />;
-// }
-
 export default function Router() {
   const router = createBrowserRouter([
     {
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,61 @@
+import "../styles/Header.css";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { GoHomeFill } from "react-icons/go";
+import { MdWork } from "react-icons/md";
+import { Link } from "react-router-dom";
+
 function Header() {
   return (
-    <>
-      <div>Header</div>
-    </>
+    <header className="header">
+      <div className="header1">
+        <div>roobbs</div>
+        <ul>
+          <li title="Home">
+            <Link to="/" className="headerIcon">
+              <GoHomeFill size={20} />
+            </Link>
+          </li>
+          <li title="Github">
+            <a href="https://github.com/roobbs" className="headerIcon">
+              <FaGithub size={20} />
+            </a>
+          </li>
+          <li title="Linkedin">
+            <a href="https://github.com/roobbs" className="headerIcon">
+              <FaLinkedin size={20} />
+            </a>
+          </li>
+          <li title="Portfolio">
+            <a href="https://roobbs.vercel.app" className="headerIcon">
+              <MdWork size={20} />
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="header2">
+        {/* <Link>BLOG</Link> */}
+        <div className="headerBlog">BLOG</div>
+        <ul className="ulButtons">
+          <li>
+            <Link className="userButton" to="/">
+              All Posts
+            </Link>
+          </li>
+          <li>
+            <Link className="userButton" to="/login">
+              Log In
+            </Link>
+          </li>
+          <li>
+            <Link className="userButton" to="/signup">
+              Sign Up
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </header>
   );
 }
 
