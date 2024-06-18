f451aa37c00a2561185008ad71901c1a642c3e69
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-06-18 15:33:49 -0600

    Added home page

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,59 +1,38 @@
 import "../styles/screens/Home.css";
-import ArticleCard from "../components/ArticleCard";
+import { Link } from "react-router-dom";
+import { MdArrowRightAlt } from "react-icons/md";
 
 function Home() {
   return (
     <main className="homeContainer">
-      <div>See my most recent posts:</div>
-      <div className="articles">
-        <ArticleCard
-          title="title"
-          text="text text text"
-          label="css"
-          date="date 3wqr k "
-        ></ArticleCard>
-        <ArticleCard
-          title="title"
-          text="text text text"
-          label="css"
-          date="date 3wqr k "
-        ></ArticleCard>
-        <ArticleCard
-          title="title"
-          text="text text text"
-          label="css"
-          date="date 3wqr k "
-        ></ArticleCard>
-        <ArticleCard
-          title="title"
-          text="text text text"
-          label="css"
-          date="date 3wqr k "
-        ></ArticleCard>
-        <ArticleCard
-          title="title"
-          text="text text text"
-          label="css"
-          date="date 3wqr k "
-        ></ArticleCard>
-        <ArticleCard
-          title="title"
-          text="text text text"
-          label="css"
-          date="date 3wqr k "
-        ></ArticleCard>
-        <ArticleCard
-          title="title"
-          text="text text text"
-          label="css"
-          date="date 3wqr k "
-        ></ArticleCard>
-        <ArticleCard
-          title="title"
-          text="text text text"
-          label="css"
-          date="date 3wqr k "
-        ></ArticleCard>
+      <div className="welcome">
+        <div className="welcomeTitle">
+          <div>Welcome to</div>
+          <div style={{ display: "flex" }}>
+            roobbs<div style={{ color: "#09cca9" }}>.BLOG</div>!
+          </div>
+        </div>
+        <div className="welcomeText">
+          Discover valuable resources and personal reflections on CSS,
+          JavaScript, and the challenging path of self-taught learning in web
+          development and programming in general.
+        </div>
+        <div className="welcomeText">
+          <Link
+            to="/signup"
+            style={{
+              paddingRight: 5,
+              color: "#09cca9",
+              textDecoration: "none",
+            }}
+          >
+            Sign up now
+          </Link>
+          and discover more user features!
+        </div>
+        <Link className="link">
+          Start reading <MdArrowRightAlt size={30} color="#09cca9" />
+        </Link>
       </div>
     </main>
   );
