8830f5c50bea76626154cea097b6d198e1244ba6
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-06-18 13:03:03 -0600

    Created footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,15 @@
+import "../styles/components/Footer.css";
+import { IoGitMergeOutline } from "react-icons/io5";
+
 function Footer() {
   return (
-    <>
-      <div>Footer</div>
-    </>
+    <footer className="footer">
+      <div>© 2024</div>
+      <IoGitMergeOutline color="#00cca7" size="18" />
+      <div style={{ display: "flex" }}>
+        roobbs <div style={{ color: "#00cca7", fontWeight: "900" }}>.BLOG</div>
+      </div>
+    </footer>
   );
 }
 
