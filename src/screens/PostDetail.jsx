d3b492c8de96ec65e510db20695c3037165292d2
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-06-19 11:42:09 -0600

    Created post detail page

--- src/screens/PostDetail.jsx
+++ src/screens/PostDetail.jsx
@@ -1,9 +1,50 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import "../styles/screens/PostDetail.css";
 
 function PostDetail() {
-  const postId = useParams();
+  const { postId } = useParams();
+  const [post, setPost] = useState(false);
+  const [comments, setComments] = useState([]);
+  const [error, setError] = useState(false);
 
-  return <div>Post detail with id: {postId}</div>;
+  useEffect(() => {
+    const fetchSinglePost = async () => {
+      try {
+        const res = await fetch(
+          `https://blog-api-production-87f1.up.railway.app/posts/${postId}`
+        );
+        const object = await res.json();
+        console.log(object);
+        setComments(object.comments);
+        setPost(object.post);
+      } catch (err) {
+        setError(err);
+        console.log(err);
+      }
+    };
+    fetchSinglePost();
+  }, [postId]);
+
+  return (
+    <div className="singlePostContainer">
+      <div>
+        {error && <div>oops there is an error</div>}
+        {!post && <div>wait...</div>}
+        {post && (
+          <div className="postContainer">
+            <div className="postTitle">{post.title}</div>
+            <div dangerouslySetInnerHTML={{ __html: post.text }}></div>
+            <div className="postAuthor">
+              {post.author.first_name + " " + post.author.last_name}
+            </div>
+          </div>
+        )}
+        {comments.lenght && <div>There are comments</div>}
+      </div>
+      <div>Hola, aqui van algunos otros posts</div>
+    </div>
+  );
 }
 
 export default PostDetail;
